5b2fe4002d10c533820a334c30d6ec36fa430b2a
piczhanf <zhangsai@MacBook-Air.local>
2024-03-06 17:54:15 +0800

    fix font error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/font';
+import { lusitana } from '@/app/ui/fonts';
 import Link from 'next/link';
 import Image from 'next/image';
 
